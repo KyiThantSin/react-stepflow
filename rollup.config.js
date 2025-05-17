01595eb47f6a7e80cfdebf39279e1a866a86b1dd
KyiThantSin <kyits485@gmail.com>
2025-05-17 14:13:27 +0700

    setup dev and config

---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import dts from 'rollup-plugin-dts';
+
+export default [
+	{
+	input : 'src/index.ts',
+		output: [
+			{
+				file: 'dist/index.js',
+				format: 'cjs',
+				sourcemap: true,
+      			exports: 'named',
+			},
+			{
+				file: 'dist/index.esm.js',
+        		format: 'esm',
+        		sourcemap: true,
+        		exports: 'named',
+			},
+		],
+		plugins : [
+			peerDepsExternal(),
+			resolve(),
+			commonjs(),
+			typescript({
+				tsconfig: './tsconfig.json',
+				exclude: ['**/__tests__/**', '**/*.test.ts', '**/*.test.tsx'],
+			}),
+			postcss({
+				extensions:['.css'],
+				minimize: true,
+				inject:{insertAt: 'top'},
+				extract:false,
+			}),
+			terser(),
+		],
+		external:['react','react-dom'],
+	},
+	{
+		input: 'src/index.ts',
+		output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+		plugins: [dts()],
+		external: [/\.css$/],
+	},
+]
