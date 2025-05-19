ba7bdafc89ea923a32ee39c8f7114749391e24ca
KyiThantSin <kyits485@gmail.com>
2025-05-19 15:59:59 +0700

    add step connector

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import Step from './components/Step/Step';
-import Stepper from './components/Stepper/Stepper';
+import Step from "./components/Step/Step";
+import Stepper from "./components/Stepper/Stepper";
+import StepConnector from "./components/StepConnector/StepConnector";
 
-export { Step, Stepper };
+export { Step, Stepper, StepConnector };
